53d3b789f27d574d563427f34b583e9244017ae5
sujeong <susu.91@daum.net>
2020-01-16 13:27:57 +0900

    sequelize설정, 디비 연결

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const db = require('./models');
+
 const app = express();
+db.sequelize.sync(); // 테이블 생성
 
 app.get('/', (req, res) => {
   res.send('hello, server');
---
+++ models/post.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  }, {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci',
+  });
+  Post.associate = (db) => {
+    db.Post.belongsTo(db.User);
+    db.Post.hasMany(db.Comment);
+    db.Post.hasMany(db.Image);
+    db.Post.belongsTo(db.Post, { as: 'Retweet' }); // RetweetId 컬럼 생겨요. as : 별명 지어준 것임. db.Post와 구분이 안되서.
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+    db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' });
+  };
+  return Post;
+};
